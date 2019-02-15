a7f3208b84a485536d3496a3f350731eae733ce9
ckopecky <ckopecky@github.com>
2019-02-14 17:03:48 -0800

    Got rid of package.lock.json (came with repo and I use yarn)
    update siteconfig to not use Disqus

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -10,7 +10,6 @@ const config = {
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
-  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "MM/DD/YYYY", // Date format for display.
